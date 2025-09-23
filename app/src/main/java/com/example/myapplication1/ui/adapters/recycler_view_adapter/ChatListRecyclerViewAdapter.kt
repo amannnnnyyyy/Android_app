3103b6ec1f1006616091b3a066be2ed525da96a8
Amanuel Moha <mohaaman922@gmail.com>
2025-09-23 13:41:31 +0400

    set contact name

--- app/src/main/java/com/example/myapplication1/ui/adapters/recycler_view_adapter/ChatListRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/myapplication1/ui/adapters/recycler_view_adapter/ChatListRecyclerViewAdapter.kt
@@ -3,9 +3,13 @@ package com.example.myapplication1.ui.adapters.recycler_view_adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication1.R
 import com.example.myapplication1.data.model.chat.Chat
+import com.example.myapplication1.data.model.contact.ContactModel
+import com.google.android.material.imageview.ShapeableImageView
 
 class ChatListRecyclerViewAdapter(chatList:List<Chat>): RecyclerView.Adapter<ChatListRecyclerViewAdapter.ChatListRecyclerViewHolder>() {
     val chats = chatList
@@ -21,8 +25,18 @@ class ChatListRecyclerViewAdapter(chatList:List<Chat>): RecyclerView.Adapter<Cha
         holder: ChatListRecyclerViewHolder,
         position: Int
     ) {
-        holder.itemView.let{
+        val chat = chats[position]
+        val profilePic = holder.itemView.findViewById<ShapeableImageView>(R.id.userProfile)
+        val name = holder.itemView.findViewById<TextView>(R.id.name)
 
+        val contact = ContactModel.contacts.find {
+            it.id == chat.sender
+        }
+
+        holder.itemView.let{
+            profilePic.setImageURI(contact?.profilePic)
+
+            name.text = contact?.name
         }
     }
 
--- app/src/main/java/com/example/myapplication1/ui/fragments/chat_list/ChatListFragment.kt
+++ app/src/main/java/com/example/myapplication1/ui/fragments/chat_list/ChatListFragment.kt
@@ -10,7 +10,6 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication1.R
 import com.example.myapplication1.data.model.chat.Chat
 import com.example.myapplication1.data.model.chat.ChatModel
-import com.example.myapplication1.data.model.message.Message
 import com.example.myapplication1.data.model.message.MessageModel
 import com.example.myapplication1.data.model.message.ReadStatus
 import com.example.myapplication1.databinding.FragmentChatListBinding
@@ -27,28 +26,14 @@ class ChatListFragment : Fragment(R.layout.fragment_chat_list) {
         val recycler = binding.recyclerView
         val bottomNav = binding.filterBtn
 
-        var adapter : ChatListRecyclerViewAdapter = ChatListRecyclerViewAdapter(ChatModel.chats)
+        val adapter = ChatListRecyclerViewAdapter(ChatModel.chats)
 
         bottomNav.setOnItemSelectedListener {item ->
             when(item.itemId){
-                R.id.all->{
-                    dynamicAdapter(ChatModel.chats,recycler)
-                }
-                R.id.fav -> {
-                    dynamicAdapter(ChatModel.chats.filter { it.favourite },recycler)
-                }
-                R.id.unread -> {
-                    dynamicAdapter(ChatModel.chats.filter { ch->
-                        MessageModel.messagesList.any { msg ->
-                            msg.readStatus == ReadStatus.NOT_READ
-                        }
-                    },recycler)
-                }
-                R.id.groups -> {
-                    dynamicAdapter(ChatModel.chats.filter { ch ->
-                        ch.group
-                    },recycler)
-                }
+                R.id.all -> dynamicAdapter(recycler,"all")
+                R.id.fav -> dynamicAdapter(recycler,"fav")
+                R.id.unread -> dynamicAdapter(recycler,"unread")
+                R.id.groups -> dynamicAdapter(recycler,"groups")
             }
             true
 
@@ -63,8 +48,17 @@ class ChatListFragment : Fragment(R.layout.fragment_chat_list) {
         return binding.root
     }
 
-    fun dynamicAdapter(chats:List<Chat>, recycler: RecyclerView){
-        val adapter = ChatListRecyclerViewAdapter(chats)
+    fun dynamicAdapter(recycler: RecyclerView, type:String){
+        val chatList: List<Chat> = when(type){
+            "all" ->    ChatModel.chats
+            "fav" ->    ChatModel.chats.filter { it.favourite }
+            "unread" -> ChatModel.chats.filter { ch->
+                            MessageModel.messagesList.any { msg -> msg.readStatus == ReadStatus.NOT_READ }
+                        }
+            "groups" -> ChatModel.chats.filter { ch -> ch.group }
+            else -> emptyList()
+        }
+        val adapter = ChatListRecyclerViewAdapter(chatList)
         recycler.adapter = adapter
         recycler.layoutManager = LinearLayoutManager(requireContext())
     }
