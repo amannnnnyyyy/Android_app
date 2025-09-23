a78ed3cb0b8c2af215c48ffe2cad0366aa59cae7
Amanuel Moha <mohaaman922@gmail.com>
2025-09-23 11:42:33 +0400

    view pager and bottom nav

--- app/src/main/java/com/example/myapplication1/ui/fragments/chat_holder/ChatHolderFragment.kt
+++ app/src/main/java/com/example/myapplication1/ui/fragments/chat_holder/ChatHolderFragment.kt
@@ -5,56 +5,48 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.get
+import androidx.viewpager2.widget.ViewPager2
+import com.example.myapplication1.Chats_display_fragment
+import com.example.myapplication1.ContactDetails
 import com.example.myapplication1.R
+import com.example.myapplication1.databinding.FragmentChatHolderBinding
+import com.example.myapplication1.ui.adapters.view_pager_adapter.ViewPager2Adapter
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class ChatHolderFragment : Fragment(R.layout.fragment_chat_holder) {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val binding = FragmentChatHolderBinding.inflate(inflater,container,false)
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ChatHolderFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class ChatHolderFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+        val bottomNavView = binding.bottomNavView
+        val viewPager2 = binding.viewPager
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+        val fragmentList = arrayListOf<Fragment>(
+            Chats_display_fragment(),
+            ContactDetails()
+        )
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat_holder, container, false)
-    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ChatHolderFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ChatHolderFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        // for view pager swipe
+        val viewPagerAdapter = ViewPager2Adapter(fragmentList, requireActivity().supportFragmentManager,lifecycle)
+        viewPager2.adapter = viewPagerAdapter
+
+        // to change bottom nav with view pager
+        bottomNavView.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.chatsWithRecycler -> viewPager2.currentItem = 0
+                R.id.contactDetails2 -> viewPager2.currentItem = 1
             }
+            true
+        }
+
+        // for bottom nav click
+        viewPager2.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                bottomNavView.menu.get(position).isChecked = true
+            }
+        })
+
+        return binding.root
     }
 }
