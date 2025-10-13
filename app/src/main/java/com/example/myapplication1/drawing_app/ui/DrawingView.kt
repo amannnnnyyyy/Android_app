4a74da99062e92e9284022928206ffb5eb921d2b
Amanuel Moha <mohaaman922@gmail.com>
2025-10-13 15:19:28 +0400

    drawing view error and layout

--- app/src/main/java/com/example/myapplication1/drawing_app/ui/DrawingView.kt
+++ app/src/main/java/com/example/myapplication1/drawing_app/ui/DrawingView.kt
@@ -8,6 +8,8 @@ import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
 import android.graphics.Paint
+import android.view.MotionEvent
+import androidx.core.graphics.createBitmap
 
 class DrawingView(context: Context, attrs: AttributeSet): View(context, attrs){
 
@@ -31,6 +33,60 @@ class DrawingView(context: Context, attrs: AttributeSet): View(context, attrs){
         setUpDrawing()
     }
 
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        canvasBitMap = createBitmap(w, h)
+        canvas  =Canvas(canvasBitMap)
+    }
+
+
+    //respond to user touches
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val touchX = event?.x
+        val touchY = event?.y
+
+        when(event?.action) {
+            //triggered when the user puts finger on the screen (sinekaw)
+            MotionEvent.ACTION_DOWN -> {
+                drawPath.color = color
+                drawPath.brushThickness = brushSize
+                drawPath.reset()
+
+                if (touchX != null && touchY != null) {
+                    drawPath.moveTo(touchX, touchY)
+                }
+            }
+            //triggered when user starts to move finger until user takes finger off (siyanfuakek)
+            MotionEvent.ACTION_MOVE -> {
+                if (touchX != null && touchY != null) {
+                    drawPath.lineTo(touchX, touchY)
+                }
+            }
+
+            //triggered when user takes off the finger (silekew)
+            MotionEvent.ACTION_UP -> {
+                drawPath = FingerPath(color, brushSize)
+            }
+
+            else -> return false
+        }
+            invalidate() //refresh the layout
+            return true
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        canvas.drawBitmap(canvasBitMap, 0f,0f,drawPaint)
+
+        if (!drawPath.isEmpty){
+            drawPaint.strokeWidth = drawPath.brushThickness
+            drawPaint.color = drawPath.color
+            canvas.drawPath(drawPath, drawPaint) //drawing path on canvas
+        }
+    }
+
     private fun setUpDrawing(){
         drawPaint = Paint()
         drawPath = FingerPath(color, brushSize)
@@ -38,9 +94,10 @@ class DrawingView(context: Context, attrs: AttributeSet): View(context, attrs){
         drawPaint.style = Paint.Style.STROKE
         drawPaint.strokeJoin = Paint.Join.ROUND
         drawPaint.strokeCap = Paint.Cap.ROUND
+
+        canvasPaint = Paint(Paint.DITHER_FLAG)
         brushSize = 20F
     }
 
-
-    internal inner class FingerPath(val color: Int, val brushTickness: Float): Path()
+    internal inner class FingerPath(var color: Int, var brushThickness: Float): Path()
 }
