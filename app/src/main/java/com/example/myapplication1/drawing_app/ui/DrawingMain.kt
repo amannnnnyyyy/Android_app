5408046ca3f9939771cde9891fe544f26d7f01e6
Amanuel Moha <mohaaman922@gmail.com>
2025-10-14 10:55:30 +0400

    perfect permission request for Images

--- app/src/main/java/com/example/myapplication1/drawing_app/ui/DrawingMain.kt
+++ app/src/main/java/com/example/myapplication1/drawing_app/ui/DrawingMain.kt
@@ -1,25 +1,56 @@
 package com.example.myapplication1.drawing_app.ui
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.Dialog
-import androidx.fragment.app.viewModels
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SeekBar
 import android.widget.TextView
-import android.graphics.Color
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.myapplication1.R
 import com.example.myapplication1.databinding.FragmentDrawingMainBinding
 import yuku.ambilwarna.AmbilWarnaDialog
+
 
 class DrawingMain : Fragment() {
 
     private val viewModel: DrawingMainViewModel by viewModels()
 
     private lateinit var drawingView: DrawingView
+
+    private var requestAskedCounter:Int = 0
+
+    private var requestPermission: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){
+            permissions->
+            permissions.entries.forEach {
+                val permissionName = it.key
+                val isGranted = it.value
+
+                if (isGranted && permissionName== Manifest.permission.READ_MEDIA_IMAGES){
+                    Toast.makeText(requireContext(), "permission granted", Toast.LENGTH_SHORT).show()
+                }else{
+                    if (permissionName == Manifest.permission.READ_MEDIA_IMAGES){
+                        Toast.makeText(requireContext(), "permission denied", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,11 +61,25 @@ class DrawingMain : Fragment() {
         drawingView = binding.draw
 
 
-        binding.purple.setOnClickListener { drawingView.setColor(R.color.purple_700) }
+        binding.purple.setOnClickListener { drawingView.setColor(R.color.purple_500) }
         binding.green.setOnClickListener { drawingView.setColor(Color.GREEN) }
         binding.black.setOnClickListener { drawingView.setColor(Color.BLACK) }
         binding.yellow.setOnClickListener { drawingView.setColor(Color.YELLOW) }
         binding.red.setOnClickListener { drawingView.setColor(Color.RED) }
+
+        binding.gallery.setOnClickListener {
+            if (
+                ActivityCompat
+                    .checkSelfPermission(
+                        requireContext(),
+                        Manifest.permission.READ_MEDIA_IMAGES)
+                != PackageManager.PERMISSION_GRANTED
+                ){
+                requestStoragePermission()
+            }else{
+
+            }
+        }
 
 
         binding.undo.setOnClickListener { drawingView.undoPath() }
@@ -90,7 +135,7 @@ class DrawingMain : Fragment() {
 
 
     private fun showColorPickerDialog(){
-        val dialog = AmbilWarnaDialog(requireContext(), drawingView.getColor(), object: AmbilWarnaDialog.OnAmbilWarnaListener{
+        AmbilWarnaDialog(requireContext(), drawingView.getColor(), object: AmbilWarnaDialog.OnAmbilWarnaListener{
             override fun onCancel(dialog: AmbilWarnaDialog?) {
             }
 
@@ -99,5 +144,71 @@ class DrawingMain : Fragment() {
             }
 
         }).show()
+    }
+
+
+
+    private fun requestStoragePermission(){
+        requestAskedCounter++
+        Log.i("hereindialog","checking if we should")
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), Manifest.permission.READ_MEDIA_IMAGES)){
+            Log.i("hereindialog","Yeah we should")
+
+            showRationaleDialog()
+        }else{
+            Log.i("hereindialog","No we shouldn't")
+
+            if (requestAskedCounter==1){
+                requestPermission.launch(
+                    arrayOf(Manifest.permission.READ_MEDIA_IMAGES)
+                )
+            }else{
+                AlertDialog.Builder(requireContext())
+                    .setTitle("Permission Denied Permanently")
+                    .setMessage("Camera permission is permanently denied. Please enable it in app settings.")
+                    .setPositiveButton(
+                        "Go to Settings",
+                        DialogInterface.OnClickListener { dialog: DialogInterface?, which: Int ->
+                            val intent: Intent =
+                                Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                            val uri: Uri? = Uri.fromParts("package", requireActivity().packageName, null)
+                            intent.setData(uri)
+                            startActivity(intent)
+                        })
+                    .setNegativeButton(
+                        "Cancel",
+                        DialogInterface.OnClickListener { dialog: DialogInterface?, which: Int -> dialog!!.dismiss() })
+                    .create()
+                    .show()
+            }
+        }
+    }
+
+
+    private fun showRationaleDialog(){
+        Log.i("hereindialog","about to request")
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("Storage permission")
+            .setMessage("Storage permission is needed to continue")
+            .setPositiveButton("Ok"){ dialog, which ->
+                requestPermission.launch(
+                    arrayOf(Manifest.permission.READ_MEDIA_IMAGES)
+                )
+                dialog.dismiss()
+            }.create().show()
+    }
+
+    private fun showManualPermissionDialog(){
+        Log.i("hereindialog","about to request")
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("Storage permission")
+            .setMessage("Storage permission is needed to continue")
+            .setPositiveButton("Ok"){ dialog, which ->
+                requestPermission.launch(
+                    arrayOf(Manifest.permission.READ_MEDIA_IMAGES)
+                )
+                dialog.dismiss()
+            }.create().show()
     }
 }
